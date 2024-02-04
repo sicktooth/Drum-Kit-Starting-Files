be588dbd0c225a727fe84200691ebbe97f1a3366
sicktooth <sosaronungwe@gmail.com>
2024-02-04 02:22:07 +0100

    updated index .js & .html

---
+++ index.js
@@ -0,0 +1,8 @@
+var numOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numOfButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I got clicked!");
+
+    });
+}
