d9b0b8ee54e5828bc80291664fa67bd4098e296b
sicktooth <sosaronungwe@gmail.com>
2024-02-05 05:41:21 +0100

    updated index.js & styles.css

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var numOfButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numOfButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked!");
+
+        this.style.color = "white";
 
     });
 }
+
+// var audio = new Audio('sounds/tom-1.mp3');
+//         audio.play();
